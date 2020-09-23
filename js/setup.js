94193a78ca67a17489c873e381920935c58d0cb7
NastyaBy <62007947+NastyaBy@users.noreply.github.com>
2020-09-23 18:54:12 +0300

    Отладка вывода количества магов.

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,16 @@
 'use strict';
 
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const WIZARD_SURNAME = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const WIZARD_COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const WIZARD_COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYSYS_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
+
+const wizardName = WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)];
+const wizardSurname = WIZARD_SURNAMES[Math.floor(Math.random() * WIZARD_SURNAMES.length)];
+const wizardCoatColor = WIZARD_COAT_COLORS[Math.floor(Math.random() * WIZARD_COAT_COLORS.length)];
+const wizardEysyColor = WIZARD_EYSYS_COLOR[Math.floor(Math.random() * WIZARD_EYSYS_COLOR.length)];
+
+
 const userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
 
@@ -15,35 +22,25 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
 
 const wizards = [
   {
-    name: WIZARD_NAMES[0] + ` ` + WIZARD_SURNAME[0],
-    coatColor: WIZARD_COAT_COLOR[0],
-    eyesColor: WIZARD_EYSYS_COLOR[0],
+    name: wizardName + ` ` + wizardSurname,
+    coatColor: wizardCoatColor,
+    eyesColor: wizardEysyColor,
   },
   {
-    name: WIZARD_NAMES[1] + ` ` + WIZARD_SURNAME[1],
-    coatColor: WIZARD_COAT_COLOR[1],
-    eyesColor: WIZARD_EYSYS_COLOR[1],
+    name: wizardName + ` ` + wizardSurname,
+    coatColor: wizardCoatColor,
+    eyesColor: wizardEysyColor,
   },
   {
-    name: WIZARD_NAMES[2] + ` ` + WIZARD_SURNAME[2],
-    coatColor: WIZARD_COAT_COLOR[2],
-    eyesColor: WIZARD_EYSYS_COLOR[2],
+    name: wizardName + ` ` + wizardSurname,
+    coatColor: wizardCoatColor,
+    eyesColor: wizardEysyColor,
   },
   {
-    name: WIZARD_NAMES[3] + ` ` + WIZARD_SURNAME[3],
-    coatColor: WIZARD_COAT_COLOR[3],
-    eyesColor: WIZARD_EYSYS_COLOR[3],
-  },
-  {
-    name: WIZARD_NAMES[4] + ` ` + WIZARD_SURNAME[4],
-    coatColor: WIZARD_COAT_COLOR[4],
-    eyesColor: WIZARD_EYSYS_COLOR[4],
-  },
-  {
-    name: WIZARD_NAMES[5] + ` ` + WIZARD_SURNAME[5],
-    coatColor: WIZARD_COAT_COLOR[5],
-    eyesColor: WIZARD_EYSYS_COLOR[5],
-  },
+    name: wizardName + ` ` + wizardSurname,
+    coatColor: wizardCoatColor,
+    eyesColor: wizardEysyColor,
+  }
 ];
 
 
@@ -60,6 +57,7 @@ const renderWizard = function (wizard) {
 const fragment = document.createDocumentFragment();
 for (let i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
+
 }
 similarListElement.appendChild(fragment);
 
